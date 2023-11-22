25618de9339e6def3b7f580528fcc19640f69cdb
kiemtran.dev <kiemtran.dev@gmail.com>
2023-11-23 00:42:40 +0700

    initial commit

---
+++ ts/cluster1/spl_mint.ts
@@ -0,0 +1,31 @@
+import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import wallet from "../wba-wallet.json"
+
+// Import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+const token_decimals = 1_000_000n;
+
+// Mint address
+const mint = new PublicKey('63YCB6pwCj7VnVn3857KbeqPXevnTiHKBpQyuNysZFbE');
+
+(async () => {
+    try {
+        // Create an ATA
+        const ata = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+
+        // Mint to ATA
+         const mintTx = await mintTo(connection, keypair, mint, ata.address, keypair.publicKey, token_decimals);
+        console.log(`Your mint txid: ${mintTx}`);
+        // Your ata is: 4Ji5xZJKq6354HmtqHMALCWdMxXvVXAHsy4qg7gQsQAn
+        // Your mint txid: umco7NX3vnwsH5Xb7CGDHUUaDcUmdkVvFYWpJA2LDP4aedMtmkqrFZgtwC3Ho1iRhUauyvPbf9EMifEECCP5Qjm
+    } catch(error) {
+        console.log(`Oops, something went wrong: ${error}`)
+    }
+})()
